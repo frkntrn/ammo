e49e50a7527205273f3e69f7df67f78d5e9a9ccf
frkntrn <frkntrn@protonmail.com>
2019-05-07 00:40:52 +0300

    Update evade-dlp.ps1

--- evade-dlp.ps1
+++ evade-dlp.ps1
@@ -1,6 +1,6 @@
 #a little function to evade dlp 
 
-function evade-dlp {$a=""; ([char[]](cat $args[0] -Encoding utf8) | %{$a += [char](65535-[int]$_)}); return $a}
+function evade-dlp {$a=""; [char[]](cat $args[0] -Encoding utf8) | %{$a += [char](65535-[int]$_)}; return $a}
 
 #Usage
 #evade-dlp data.txt > encoded.txt
