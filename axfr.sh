1c2b2f0aa8ccffd37faccef598e9c725218f682d
frkntrn <frkntrn@protonmail.com>
2019-06-17 11:23:47 +0300

    Update axfr.sh

--- axfr.sh
+++ axfr.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
-	echo "[*] Usage: $0 <hostname>"
-fi
-
 for ns in $(host -t ns $1 | cut -d" " -f4); do
 	host -l $1 $ns | grep "has address" | cut -d" " -f1,4
 done
